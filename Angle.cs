e7da68814385d4bac30556e7000407abf776e56d
Ilya Markov <imarkovkin@gmail.com>
2022-11-21 01:20:55 +0300

    Добавьте файлы проекта.

---
+++ Angle.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_10
+{
+    class Angle
+    {
+        int degree;
+        int minute;
+        int second;
+        
+        public bool isCorrect = true;
+        public int Degree
+        {
+            get
+            {
+                return degree;
+            }
+            set
+            {
+                if (value < 0)
+                    isCorrect = false;
+                else
+                    degree = value % 360;
+            }
+        }
+        public int Minute
+        {
+            get
+            {
+                return minute;
+            }
+            set
+            {
+                if (value < 0)
+                    isCorrect = false;
+                else
+                    Degree += value / 60;                    
+                    minute = value;
+            }
+        }
+        public int Second
+        {
+            get
+            {
+                return second;
+            }
+            set
+            {
+                if (value < 0)
+                    isCorrect = false;
+                else
+                    Minute += value / 60;
+                second = value;
+            }
+        }
+                  
+        
+        public Angle (int degree, int minute, int second)
+        {
+            this.Degree = degree;
+            this.Minute = minute;
+            this.Second = second;                       
+        }
+                
+
+        public int toRadians()
+        {
+            return degree/180+minute/(180*60)+second/(180*60*60);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Angle angle;
+            do
+            {
+                Console.WriteLine("Введите градусы угла");
+                int deg = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Введите минуты угла");
+                int min = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Введите cекунды угла");
+                int sec = Convert.ToInt32(Console.ReadLine());
+
+                angle = new Angle(deg, min, sec);
+            }
+            while (angle.isCorrect == false);
+            int radians = Convert.ToInt32(Math.PI)*angle.toRadians();
+            Console.WriteLine("Угол в радианах равен:");
+            Console.WriteLine(radians);
+            Console.ReadKey();
+                
+
+            
+
+            
+
+        }
+    }
+}
